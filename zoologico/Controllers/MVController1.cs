01daeaefcfa161649c7bae9b3fe3ee82d37c36fe
manuvarella <manuelacarvalho12@icloud.com>
2023-11-17 18:25:44 -0300

    atividade css

---
+++ zoologico/Controllers/MVController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace zoologico.Controllers
+{
+    public class MVController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ zoologico/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace zoologico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
